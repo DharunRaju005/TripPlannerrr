29e773c7806599ea6cd3c32f8fdcf5c24958b135
DharunRaju005 <dharunraju22@gmail.com>
2024-10-27 22:32:08 +0530

    fixed bug:app.jsx modified

--- Client/src/App.jsx
+++ Client/src/App.jsx
@@ -2,8 +2,8 @@
 // eslint-disable-next-line no-unused-vars
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import TripForm from '../src/components/form'
-import AttractionsPage from './components/Attractions'
+import TripForm from '../src/components/form';
+import AttractionsPage from './components/Attractions';
 import AttractionDescriptionPage from './components/Attractions_Description';
 import './index.css';
 import ShortestPathPage from './components/ShortestPath';
@@ -11,25 +11,27 @@ import 'leaflet/dist/leaflet.css';
 import Login from './components/login';
 import SignUp from './components/SignUp';
 import { AuthProvider } from './hooks/useAuth'; // Import AuthProvider if needed
+import { AttractionsProvider } from './context/Attractionscontext'; // Import AttractionsProvider
 
 function App() {
   return (
     <AuthProvider>
       <Router>
+        <AttractionsProvider>
+          <Routes>
+            <Route path="/attractions" element={<AttractionsPage />} />
+            <Route path="/attractions_description" element={<AttractionDescriptionPage />} />
+            <Route path="/shortest-path" element={<ShortestPathPage />} />
+          </Routes>
+        </AttractionsProvider>
         <Routes>
           <Route path="/" element={<TripForm />} />
-          <Route path="/attractions" element={<AttractionsPage />} />
-          <Route path="/attractions_description" element={<AttractionDescriptionPage />} />
-          <Route path="/shortest-path" element={<ShortestPathPage />} />
           <Route path="/login" element={<Login />} />
           <Route path="/signup" element={<SignUp />} />
-          <Route path="*" element={<h1>404 Not Found</h1>} /> {/* Optional */}
         </Routes>
       </Router>
     </AuthProvider>
   );
 }
 
-
 export default App;
-
